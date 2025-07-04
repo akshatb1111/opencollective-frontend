bb47a6e7ad426015b9c996036fd08ceb7ff5f516
Akash Bhandari <akshatb1111@gmail.com>
2025-07-05 00:22:22 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,30 @@
+import { matchRoutes } from 'react-router-dom';
+import { initializeFaro, createReactRouterV6DataOptions, ReactIntegration, getWebInstrumentations, } from '@grafana/faro-react';
+import { TracingInstrumentation } from '@grafana/faro-web-tracing';
+
+initializeFaro({
+  url: 'https://faro-collector-prod-ap-south-1.grafana.net/collect/a6e0b8061d4a27e0d6e364aa499e5bb5',
+  app: {
+    name: 'Test ',
+    version: '1.0.0',
+    environment: 'production'
+  },
+  
+  instrumentations: [
+    // Mandatory, omits default instrumentations otherwise.
+    ...getWebInstrumentations(),
+
+    // Tracing package to get end-to-end visibility for HTTP requests.
+    new TracingInstrumentation(),
+
+    // React integration for React applications.
+    new ReactIntegration({
+      router: createReactRouterV6DataOptions({
+        matchRoutes,
+      }),
+    }),
+  ],
+});
 import { getServerSideProps, HomePage } from './home';
 
 // ts-unused-exports:disable-next-line
